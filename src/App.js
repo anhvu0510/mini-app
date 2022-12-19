875a1a09d8ccf94e570aa2e994794cabe7769bd9
vula <laanhvu99@gmail.com>
2022-12-19 20:15:41 +0700

    Add tailwind config

--- src/App.js
+++ src/App.js
@@ -1,32 +1,16 @@
-import { ToDoList } from './components/ToDoList';
-import { ToDoDescription } from './components/ToDoItem';
-import { BaseButton } from './components/Button';
-import { useRef } from 'react';
+import { ToDoList } from "./components/ToDoList";
+import { ToDoDescription } from "./components/ToDoItem";
+import { BaseButton } from "./components/Button";
+import { useRef } from "react";
 
 // Sử dụng bình thường
 function App() {
-    // const [state, dispatch] = useStore(StoreContext)
-    return (
-        <div className="app">
-            <BaseButton
-                onHanlerClick={() => {
-                    console.log('OKE');
-                }}
-            >
-                {' '}
-                Button 001{' '}
-            </BaseButton>
-            <BaseButton> Button 002</BaseButton>
-            <BaseButton> Button 003 </BaseButton>
-        </div>
-        // <div className="app">
-        //     <h1 className="app__title">Todo APP</h1>
-        //     <div className="app__content">
-        //         <ToDoList type="TODO" title="Công việc" />
-        //         <ToDoDescription title="Mô tả" />
-        //     </div>
-        // </div>
-    );
+  // const [state, dispatch] = useStore(StoreContext)
+  return (
+    <div className="app">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
 }
 
 export default App;
